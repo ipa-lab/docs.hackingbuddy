c5b0493ee006665a05a35f9ea72bc6b3de9c1bcd
Andreas Happe <andreashappe@snikt.net>
2024-05-11 21:58:55 +0200

    initial commit

---
+++ src/lib/navigation.ts
@@ -0,0 +1,34 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'Core concepts',
+    links: [
+      { title: 'Project Layout', href: '/docs/understanding-caching' },
+      { title: 'Python Infrastructure', href: '/docs/understanding-caching' },
+      { title: 'Use-Cases', href: '/docs/predicting-user-behavior', },
+      { title: 'Configuration Magic', href: '/docs/basics-of-time-travel' },
+      { title: 'Capabilities', href: '/docs/introduction-to-string-theory', },
+    ],
+  },
+  {
+    title: 'Example Usecases',
+    links: [
+      { title: 'Minimal Use-Case', href: '/docs/writing-plugins' },
+      { title: 'Including a Use-Case in a Use-Case', href: '/docs/neuralink-integration' },
+      { title: 'Using multiple LLMs', href: '/docs/temporal-paradoxes' },
+      { title: 'Linux Priv-Esc', href: '/docs/testing' },
+    ],
+  },
+  {
+    title: 'Contributing',
+    links: [
+      { title: 'How to contribute', href: '/docs/how-to-contribute' },
+    ],
+  },
+]
