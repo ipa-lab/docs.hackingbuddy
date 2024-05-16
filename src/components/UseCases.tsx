d65f53280f16c1c875747dc9cefac17969787447
Andreas Happe <andreashappe@snikt.net>
2024-05-16 19:25:09 +0200

    add type hints

--- src/components/UseCases.tsx
+++ src/components/UseCases.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
 import imgMinimal from '@/images/usecases/minimal.png'
 import imgLinux from '@/images/usecases/linux_privesc.png'
 import imgWeb from '@/images/usecases/web_pentest.png'
-import { Icon } from '@/components/Icon'
 
 export function UseCases({ children }: { children: React.ReactNode }) {
   return (
@@ -13,11 +12,11 @@ export function UseCases({ children }: { children: React.ReactNode }) {
   )
 }
 
-const images = {
+const images: { [key: string]: StaticImageData } = {
   minimal: imgMinimal,
   linux: imgLinux,
   web: imgWeb,
-}
+};
 
 export function UseCase({
   title,
@@ -32,17 +31,17 @@ export function UseCase({
 }) {
   return (
     <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-        <Image src={images[icon]} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover"/>
-        <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+      <Image src={images[icon]} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover"/>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+      <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
 
-        <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
-          <a href={href} className="text-white no-underline hover:underline">
-            <span className="absolute inset-0"></span>
-            {title}
-          </a>
-        </h3>
-        <span className="text-white">{description}</span>
+      <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
+        <a href={href} className="text-white no-underline hover:underline">
+          <span className="absolute inset-0"></span>
+          {title}
+        </a>
+      </h3>
+      <span className="text-white">{description}</span>
     </div>
   )
 }
